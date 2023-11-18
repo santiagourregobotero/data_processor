3f087f0cbb91c1cd1e5d1cf1db9a854e179486ff
santiagourregobotero <santiagourregobotero@gmail.com>
2023-11-19 06:05:03 +0900

    remove some comments

--- mainframe/impl/frame.hpp
+++ mainframe/impl/frame.hpp
@@ -1,7 +1,6 @@
 //          Copyright Santiago Urrego Botero 2022.
 // Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          https://www.boost.org/LICENSE_1_0.txt)
+
 
 #ifndef INCLUDED_mainframe_frame_impl_h
 #define INCLUDED_mainframe_frame_impl_h
