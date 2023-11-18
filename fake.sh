7ba04f485faf3238f4e71ed34420797b8ac7c4f0
santiagourregobotero <santiagourregobotero@gmail.com>
2023-11-19 05:49:48 +0900

    Finalize

---
+++ fake.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_EMAIL="middleton.ted@gmail.com"
+CORRECT_NAME="santiagourregobotero"
+CORRECT_EMAIL="santiagourregobotero@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
